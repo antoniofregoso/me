1f26ec5452eddc4c991cca2b53f9623fb4297dbc
antoniofregoso <yomero@antoniofregoso.com>
2020-10-05 03:52:38 -0500

    [FIX] temp splution for read me.ini file

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     config = configparser.ConfigParser() 
     path_config_file = os.path.join(basedir , 'me.ini')
-    config.read(path_config_file)
+    config.read('/var/www/me/me/me.ini')
     OPTIONS = config['options']
     ODOO = config['odoo']
     CRM = config['crm']
     csrf = OPTIONS['csrf_secret'] 
-    SECRET_KEY = os.environ.get('SECRET_KEY') or csrf 
+    SECRET_KEY = os.environ.get('SECRET_KEY') or csrfs
