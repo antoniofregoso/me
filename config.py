8313aa90a6ded5ec0e1a5ef576e0b9f6051fe9d1
antoniofregoso <yomero@antoniofregoso.com>
2020-10-05 04:00:02 -0500

    [FIX] UTFO in config

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import os
 import configparser
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -5,9 +6,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     config = configparser.ConfigParser() 
     path_config_file = os.path.join(basedir , 'me.ini')
-    config.read('/var/www/me/me/me.ini')
+    config.read(path_config_file)
     OPTIONS = config['options']
     ODOO = config['odoo']
     CRM = config['crm']
     csrf = OPTIONS['csrf_secret'] 
-    SECRET_KEY = os.environ.get('SECRET_KEY') or csrfs
+    SECRET_KEY = os.environ.get('SECRET_KEY') or csrf 
