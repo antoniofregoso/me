8229df3a52d5200b0e69d55ac920b77b1c6634e2
antoniofregoso <yomero@antoniofregoso.com>
2020-10-05 04:02:49 -0500

    [FIX]

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ import configparser
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    config = configparser.ConfigParser() 
+    config = configparser.ConfigParser()
     path_config_file = os.path.join(basedir , 'me.ini')
     config.read(path_config_file)
     OPTIONS = config['options']
     ODOO = config['odoo']
     CRM = config['crm']
-    csrf = OPTIONS['csrf_secret'] 
-    SECRET_KEY = os.environ.get('SECRET_KEY') or csrf 
+    csrf = OPTIONS['csrf_secret']
+    SECRET_KEY = os.environ.get('SECRET_KEY') or csrf
